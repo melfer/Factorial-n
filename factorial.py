6b35bdac049a101581160544ec094933a147737a
melfer <melferalvarezsierra2015@gmail.com>
2020-09-26 23:41:29 -0500

    factorial n

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial (n):
+
+    if n== 0 or n== 1:
+
+        resultado = 1 
+        print(resultado)
+        elif n > 1:
+            resultado = n * factorial ( n-1 )
+            print(resltado)
+            return resultado
+            ress=factorial( 4 )
+            print(ress)
